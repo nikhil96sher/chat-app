d1e4ad469f5da97c506a6d593efd20e9f098cca1
NIKHIL <none>
2015-12-03 00:43:49 +0530

    User can add a nick

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var bodyParser = require('body-parser');
+var people = {}
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+app.use(bodyParser.json());
 
-app.get('/', function (req, res)
+app.get('/', function (request, res)
 {
   res.sendFile(__dirname + '/index.html');
 });
 
-/*app.get(,function (req,res) {
-	res.sendFile(__dirname + url);
-})
-*/
 io.on('connection', function(socket){
+  
+  socket.on('join',function(person)
+  {
+    var msg=person + ' has joined';
+    io.emit('chat-message',msg);
+    console.log(msg);
+    //people.append(person);
+  });
+
   socket.on('chat-message', function(msg){
     io.emit('chat-message', msg);
     console.log('Message : '+msg);
